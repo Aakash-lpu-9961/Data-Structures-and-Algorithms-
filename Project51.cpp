c4397df030f1b99c3478c29a3c8dea3d0e24373c
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-10-31 00:49:47 +0530

    Bubble Sort

--- Project51.cpp
+++ Project51.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
-int BubbleSort(int arr[], int n)
+void BubbleSort(int arr[], int n)
 {
+   for (int i = 0; i < (n-1); i++)
+   {
+    for (int j = 0; j < (n-i-1); j++)
+    {
+        if (arr[j]>arr[j+1])
+        {
+            swap(arr[j], arr[j+1]);
+        }  
+    }
     
+   }
+   
 }
 int main()
 {
@@ -10,5 +21,10 @@ int main()
     int arr[] = {12, 45, 34, 67, 87, 44, 42, 99, 54};
     int n = sizeof(arr)/sizeof(int);
     BubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
     return 0;
 }
