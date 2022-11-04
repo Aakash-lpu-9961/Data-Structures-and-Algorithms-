052e26641dd4a71c71c69c32b9861047cec05697
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-11-05 01:12:46 +0530

    Insertion Sort

--- Program57.cpp
+++ Program57.cpp
@@ -1,8 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = (i - 1);
 
-int main(){
-     //Insertion Sort Algorithm in C++
-     
-     return 0;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    // Insertion Sort Algorithm in C++
+    int arr[] = {21, 43, 11, 21, 43, 23, 45, 66, 88, 44, 32};
+    int n = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << "\n"
+         << endl;
+    InsertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << "\n"
+         << endl;
+    return 0;
 }
