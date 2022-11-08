32c879a4fce2c7b79b4f722b025e0d5c1e7c1ca0
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-11-08 20:34:25 +0530

    Pointer to Structure

--- Program61.cpp
+++ Program61.cpp
@@ -4,11 +4,16 @@ using namespace std;
 struct student
 {
     int roll;
-};
+    int age;
+}stu;
 
 
 int main(){
      //Structures in C++
-
+    //struct student stu;
+    struct student *ptr = &stu;
+    ptr->age=20;
+    ptr->roll=40;
+    cout<<ptr->age<<" "<<ptr->roll;
      return 0;
 }
