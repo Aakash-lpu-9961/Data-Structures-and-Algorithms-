96b1535f956a8bfdc4937c2282c784fcabfc25ce
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-11-19 16:23:48 +0530

    Linked List

---
+++ Program83.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+node *insertBegin(node *head, int x)
+{
+    node *temp = new node(x);
+    temp->next = head;
+    return temp;
+}
+void printlist(node *head)
+{
+    node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+}
+int main()
+{
+    int x;
+    node *head;
+    head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next = new node(40);
+    head->next->next->next->next = new node(50);
+
+    cout << "Enter Value of node to insert:"
+         << " ";
+    cin >> x;
+
+    head = insertBegin(head, x);
+    printlist(head);
+}
